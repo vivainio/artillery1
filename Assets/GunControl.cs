0e14c245129fc48f3b3056b46e460a6d8967dee3
Ville Vainio <vivainio@Villes-MacBook-Pro.local>
2014-08-20 10:24:36 +0300

    First commit (with some game already)

---
+++ Assets/GunControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var k = Input.GetKey (KeyCode.A);
+		if (k) {
+			Debug.Log("A pressed");
+			
+		}
+	
+	}
+
+	void FixedUpdate() {
+
+	}
+
+}
---
+++ Assets/Launcheri.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcheri : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var c = GameObject.Find ("Cube");
+		Debug.Log ("hello world" + c);
+		c.rigidbody.AddForce (new Vector3 (200, 200));
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var c = GameObject.Find ("Cube");
+
+
+
+		var inp = Input.GetMouseButtonDown (0);
+		if (inp) {
+			//c.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+			var v = c.rigidbody.velocity;
+			c.rigidbody.AddForce( new Vector3(-400, 600));
+
+			Debug.Log ("Pressed! " + v);
+
+		}
+
+
+	
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		var c = GameObject.Find ("Cube");
+		c.rigidbody.AddExplosionForce (800, c.transform.position, 800);
+		//var cyl = GameObject.Find ("Cylinder");
+		//cyl.rigidbody.AddForce(new Vector2(900, 900));
+		Debug.Log ("collision");
+
+
+	}
+	
+}
